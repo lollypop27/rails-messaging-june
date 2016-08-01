18f049d062452980fa02d7d8830b80db2dc356a8
lollypop27 <lara@adfactory.co.za>
2016-08-01 06:29:00 +0200

    WIP adding common password Scenario

---
+++ features/step_definitions/nav_steps.rb
@@ -0,0 +1,27 @@
+# Given(/^I am on the "([^"]*)" page$/) do |pagename|
+#   visit path(pagename)
+# end
+#
+# Then(/^I should be on the "([^"]*)" page$/) do |pagename|
+#   expect(page.current_path).to eq path(pagename)
+# end
+#
+# def path(pagename)
+#   case pagename
+#     when "signup"
+#       new_user_registration_path
+#     when "home"
+#       root_path
+#     else
+#       raise "Path to #{pagename} page is not specified"
+#   end
+# end
+
+def path(pagename)
+  case pagename
+    when "login"
+      user_session_path
+    when "forgot password"
+      new_user_password_path
+  end
+end
